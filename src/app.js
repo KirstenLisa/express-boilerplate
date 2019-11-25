08a11184d2f182aa6c4532b41719e82494418df5
Kirsten Kustusch <kirsten.kustusch@gmail.com>
2019-11-25 11:32:19 +0200

    fix

--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const { NODE_ENV } = require('./config')
+const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
